2d55411d9d3abf8d68e6dc54ff1df4cd73fc5e54
Ryan Replogle <ryan.replogle@gmail.com>
2023-09-14 22:56:22 -0400

    set up dashboard methods

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ChartServiceService, JioMart } from 'src/services/chart-service.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  dashboardData = new BehaviorSubject<JioMart[]>([]);
+  groceries: JioMart[] = [];
+  constructor(public chartService: ChartServiceService) {}
 
+  async ngOnInit(): Promise<void> {
+    this.dashboardData.next(await this.chartService.fetchDashboardData(''));
+    //this.groceries = await this.chartService.fetchDashboardData('Groceries');
+  }
+
+  async updateDashboard(filter: string): Promise<void> {
+    this.dashboardData.next([]);
+    this.dashboardData.next(await this.chartService.fetchDashboardData(filter));
+  }
 }
