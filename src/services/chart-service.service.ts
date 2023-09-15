f66e02fd5a781c33d53bf2a3a26f3f16c515c153
Ryan Replogle <ryan.replogle@gmail.com>
2023-09-14 23:03:20 -0400

    set up dashboard methods (#7)

--- src/services/chart-service.service.ts
+++ src/services/chart-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as arrow from 'apache-arrow';
+import { BehaviorSubject } from 'rxjs';
 import { DatabaseManager } from 'src/db-manager';
 
 export interface JioMartData {
@@ -10,14 +11,23 @@ export interface JioMartData {
   category_count: number,
 }
 
+export interface JioMart {
+  category: string,
+  sub_category: string,
+  items: string,
+  price: number,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ChartServiceService {
-
+  queryExecutionTime = new BehaviorSubject<string>('');
   constructor(private dbManager: DatabaseManager) { }
 
   async fetchData(): Promise<JioMartData[]> {
+    const startTime = performance.now()
+
     const sql = `
     select
         items.category
@@ -39,6 +49,40 @@ export class ChartServiceService {
       }>(sql);
     }
 
+
+    var endTime = performance.now();
+    this.queryExecutionTime.next(`${endTime - startTime} milliseconds`);
     return result.toArray().map((row) => row as JioMartData);
+  }
+
+  async fetchDashboardData(filter: string): Promise<JioMart[]> {
+    const startTime = performance.now();
+    const whereSql = this.getWhereSql(filter);
+    const sql = `
+    select * from items ${whereSql} limit 2000;`;
+
+    let result!: arrow.Table;
+    for await (const conn of this.dbManager.open()) {
+      result = await conn.query<{
+        category: arrow.Utf8;
+        sub_category: arrow.Utf8;
+        items: arrow.Utf8;
+        price: arrow.Decimal;
+      }>(sql);
+    }
+
+    var endTime = performance.now();
+    this.queryExecutionTime.next(`${endTime - startTime} milliseconds`);
+    return result.toArray().map((row) => row as JioMart);
+  }
+
+  private getWhereSql(filter: string) {
+    let whereSql = 'where 1=1 ';
+
+    if (filter !== '') {
+      whereSql += ` and items.category = '${filter}'`;
+      return whereSql;
+    }
+    return whereSql;
   }
 }
